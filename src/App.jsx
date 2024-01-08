73e943c1a6c913ccc572fc9cca6d142ad20a8522
Kevin Cortés <jefercort@gmail.com>
2024-01-08 15:51:01 -0500

    Se hizo el operador ternario para traer al main la lista de peliculas provenientes del Json que descargamos como respuesta

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css'
+import responseMovies from "./mocks/with-results.json"
+import withoutResults from "./mocks/no-results.json"
 
 function App() {
+  const movies = responseMovies.Search
+  const hasMovies = movies?.length > 0 
+
   return (
     <div className='page'>
 
@@ -12,7 +17,26 @@ function App() {
         </form>
       </header>
       <main>
-        Van los resultados
+        {
+          hasMovies ? (
+            <ul>
+              {
+                // al realizar este .map podemos ahorrar mucho tiempo ya que de una vez sabemos como es la estructyra de la respuesta sin 
+                // haber tenido que hacer un fetch
+                movies.map((movie) => (
+                  <li key={movie.imdbID}>
+                    <h3>{movie.Title}</h3>
+                    <p>{movie.Year}</p>
+                    <img src={movie.Poster} alt={movie.Title} />
+                  </li>
+                ))
+              }
+            </ul>
+          ) :
+          (
+            <p>No se encontraro peliculas para esta busqueda</p>
+          )
+        }
       </main>
 
     </div>
